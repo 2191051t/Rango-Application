bb638a43e9369bd9512f1be3112df2c46c685b3b
Greg Tucker <2191051t@student.gla.ac.uk>
2017-02-08 14:17:24 +0000

    chapter 3 with fixed about

--- rango/models.py
+++ rango/models.py
@@ -1,32 +1,5 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.template.defaultfilters import  slugify
-
 
 # Create your models here.
-class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
-    views = models.IntegerField(default=0)
-    likes = models.IntegerField(default=0)
-    slug = models.SlugField(unique=True)
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.name)
-        super(Category, self).save(*args, **kwargs)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name_plural = 'Categories'
-
-
-class Page(models.Model):
-    category = models.ForeignKey(Category)
-    title = models.CharField(max_length=128)
-    url = models.URLField()
-    views = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.title
--- rango/views.py
+++ rango/views.py
@@ -1,28 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rango.models import Category
-from rango.models import Page
-
-
-def show_category(request, category_name_slug):
-    context_dict = {}
-    try:
-        category = Category.objects.get(slug=category_name_slug)
-        pages = Page.objects.filter(category=category)
-        context_dict['pages'] = pages
-        context_dict['category'] = category
-    except Category.DoesNotExist:
-        context_dict['category'] = None
-        context_dict['pages'] = None
-    return render(request, 'rango/category.html', context=context_dict)
-
 
 def index(request):
-    category_list = Category.objects.order_by('-likes')[:5]
-    pages_list = Page.objects.order_by('-views')[:5]
-    context_dict = {'categories': category_list, 'pages': pages_list}
-    return render(request, 'rango/index.html', context=context_dict)
-
+    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
 
 def about(request):
-    return render(request, 'rango/about.html', context=None)
+    return HttpResponse("Rango says here is the about page.<a href='/rango/'>Index</a>")
